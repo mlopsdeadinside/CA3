ada8ce7907db76ba299d199c0ca861a5ffb8bcbe
Aashan47 <aashanjaved567@gmail.com>
2023-09-05 20:49:18 +0500

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,7 +1,13 @@
-from hello import add
+from hello import add , div , mul , sub
 
 def test_add():
     assert 4 == add(2,2)
     
 def test_div():
-    assert 2 == div(4,2)        
+    assert 2 == div(4,2) 
+    
+def test_mul():
+    assert 4 == mul(2,2)
+    
+def test_sub():
+    assert 2 == sub(4,2)               
