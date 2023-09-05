20dc10de53bc7f7d162ede6a7b53492eea494933
Aashan47 <aashanjaved567@gmail.com>
2023-09-05 20:24:38 +0500

    Implement the div feature

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def add(x,y):
     return x+y
 
-def sub(x,y):
-    return x-y
+def div(x,y):
+    return x/y
--- test_hello.py
+++ test_hello.py
@@ -3,3 +3,5 @@ from hello import add
 def test_add():
     assert 4 == add(2,2)
     
+def test_div():
+    assert 2 == div(4,2)        
