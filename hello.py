eb49243b08d37edda2d825bf8e12c686ee14e853
TehreemArbab <tehreemarbab7@gmail.com>
2023-09-19 18:06:37 +0500

    sub functionality in flask

--- hello.py
+++ hello.py
@@ -1,5 +1,31 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
 def add(x,y):
     return x+y
 
+def mul(x,y):
+    return x*y
+
 def sub(x,y):
     return x-y
+
+def div(x,y):
+    return x/y
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    print("I am inside hello world")
+    return 'Hello World! I can make change at route: /change'
+
+# Define routes for calculator operations
+@app.route('/calc/sub/<int:num1>/<int:num2>', methods=['GET'])
+def calculate_sub(num1, num2):
+    result = sub(num1, num2)
+    return jsonify({"result": result})
+
+if __name__ == '__main__':
+    app.run(port=5000)
